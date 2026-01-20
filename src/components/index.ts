a538fbd956c7b31d95df51a223e55e2c30eacb13
Michael Gutensohn <michael@gutensohn.me>
2026-01-20 12:39:28 -0800

    0.1.5

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Button, type ButtonProps } from './Button'
 export { Input, type InputProps } from './Input'
+export { Dialog, type DialogProps } from './Dialog'
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './index.css'
 // Components
 export { Button, type ButtonProps } from './components/Button'
 export { Input, type InputProps } from './components/Input'
+export { Dialog, type DialogProps } from './components/Dialog'
 export {
   Command,
   CommandRoot,
